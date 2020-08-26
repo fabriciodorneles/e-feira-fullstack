7a88d053864fe3734b14ec3df3048dab4b0c54e5
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-08-26 20:27:17 -0300

    first commit

---
+++ server-e-feira/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+// src/server.ts
+import routes from './routes';
+
+const app = express();
+
+app.get('/', (request, response) => response.json({ message: 'hello world' }));
+
+app.listen(3333, () => {
+  console.log('🏹 Server started on port 3333'); // emoji WIN + .
+});
