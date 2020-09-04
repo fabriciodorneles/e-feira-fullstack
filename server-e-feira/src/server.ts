f123a0590dbdeb8d712219e57e8afbb58615697b
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-09-03 21:20:45 -0300

    cors

--- server-e-feira/src/server.ts
+++ server-e-feira/src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './typeorm/migrations';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
